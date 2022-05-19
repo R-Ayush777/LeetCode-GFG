14984872eefdbf5dc3847e8535f3cc3ef7e7fa97
Ayush Rudani <78534043+R-Ayush777@users.noreply.github.com>
2022-05-19 15:44:35 +0530

    Added solution - LeetHub

---
+++ Transform String - GFG/transform-string.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    int transform (string A, string B)
+    {
+        int ans = 0;
+        int i = B.size()-1;
+        int j = A.size()-1;
+        
+        for(; j>=0 ; j--){
+            if(A[j] != B[i])
+                ans++;
+            else
+                i--;
+        }
+        
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        
+        if(A!=B)
+            return -1;
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main () 
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string A, B; 
+        cin >> A >> B;
+        Solution ob;
+        cout <<ob.transform (A, B) << endl;
+    }
+}  // } Driver Code Ends
