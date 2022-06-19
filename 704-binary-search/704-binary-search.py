31f16e9bc527985aa23dd10516d5bd9133784161
Ayush Rudani <78534043+R-Ayush777@users.noreply.github.com>
2022-06-19 12:23:03 +0530

    Time: 249 ms (94.07%), Space: 15.5 MB (71.47%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -3,10 +3,10 @@ class Solution:
         l, r = 0, len(nums)-1
         while l <= r:
             mid = (l+r)//2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 l = mid+1
-            else:
+            elif nums[mid] > target:
                 r = mid-1
+            else:
+                return mid
         return -1
