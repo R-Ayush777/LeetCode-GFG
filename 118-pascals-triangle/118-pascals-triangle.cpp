89ffb9c1d6b708b91e9fc1b6b3b994c5eb3d3270
Ayush Rudani <78534043+R-Ayush777@users.noreply.github.com>
2022-07-19 17:19:26 +0530

    Time: 3 ms (38.93%), Space: 6.5 MB (31.97%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int rows) {
+        vector<vector<int>> triangle;
+        for(int i=0;i<rows;i++){
+            vector<int> temp(i+1,1);
+            for(int j=1;j<i;j++){
+                temp[j]=triangle[i-1][j]+triangle[i-1][j-1];
+            }
+            triangle.push_back(temp);
+        }
+        return triangle;
+    }
+};
